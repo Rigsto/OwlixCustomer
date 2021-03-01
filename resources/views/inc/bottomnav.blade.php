3207a8adc97c130802038b6635a456123627528d
Filbert <filberthtwn@gmail.com>
2021-03-01 16:41:53 +0700

    Image bug fixes

--- resources/views/inc/bottomnav.blade.php
+++ resources/views/inc/bottomnav.blade.php
@@ -4,21 +4,21 @@
             <img src="{{ asset('img/bottomBanner.svg') }}" alt="banner" class="w-100 rounded-images">
         </div>
         <div class="row justify-content-between align-items-center py-3" style="text-align: center;">
-            <div class="col-md-3"><img src="{{ asset('img/Value1.svg') }}" alt="value1">
+            <div class="col-md-3"><img src="{{ asset('img/value1.svg') }}" alt="value1">
                 <h5>Harga bisa Diadu</h5>
                 <p>Temukan berbagai produk di Owlix
                     dengan harga terjangkau dan kualitas
                     yang OKE yang membuat belanja
                     kamu makin asik setiap hari.</p>
             </div>
-            <div class="col-md-3"><img src="{{ asset('img/Value2.svg') }}" alt="value1">
+            <div class="col-md-3"><img src="{{ asset('img/value2.svg') }}" alt="value1">
                 <h5>Membantu sesama</h5>
                 <p>Selain membeli produk Owlix ,
                     kamu juga membantu dan mendukung
                     para UMKM ataupun Produk Lokal
                     untuk terus maju dan berkembang.</p>
             </div>
-            <div class="col-md-3"><img src="{{ asset('img/Value3.svg') }}" alt="value1">
+            <div class="col-md-3"><img src="{{ asset('img/value3.svg') }}" alt="value1">
                 <h5>Lengkap dan Praktis</h5>
                 <p>Apapun yang kamu cari semua
                     ada disini . kamu juga dapat
